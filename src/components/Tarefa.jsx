58512d2052e277988644909604648301d8602dbe
Wellington <welltecnic@hotmail.com>
2026-09-01 11:52:00 -0300

    feat: funcionalidades de tarefas

--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -1,4 +1,6 @@
 import {useState, useEffect} from 'react'
+import Contador from './Contador'
+import '../css/estilo.css'
 
 const Tarefa = () => {
 
@@ -16,12 +18,59 @@ const Tarefa = () => {
         localStorage.setItem("item-tarefa",JSON.stringify(tarefas))
     },[tarefas])
 
+    //FUNÇÃO ADICIONAR TAREFA
+    const  AdicionarTarefa =(e)=>{
+      //Previne que a página se recarregue automaticamente
+      e.preventDefault();
+      //Valida se o campo estiver vazio
+      if(!campo.trim()) return;
+
+      //novo objeto
+      const novaTarefa={
+        id: Date.now(),
+        texto:campo,
+      }
+      setTarefas([...tarefas,novaTarefa]);
+      setCampo('');
+    }
+    //FUNÇÃO REMOVER TAREFA
+    const RemoverTarefa=(id)=>{
+      //VERIFICA SE O ID DA TAREFA ATUAL É DIFERENTE DO ID QUE DESEJA APAGAR
+      //SE O ID FOR IGUAL(TAREFA QUE DESEJA APAGAR) A CONDIÇÃO RETORNA FALSO 
+      //E O ITEM É EXCLUIDO
+      const apagarTarefa = tarefas.filter((tarefa)=> tarefa.id !== id);
+      setTarefas(apagarTarefa);
+
+    }
 
 
   return (
-    <>
+    <div className="todo-container">
+      <h1>Minha Lista de Tarefas</h1>
+      <form onSubmit={AdicionarTarefa}>
+        <input
+          type="text"
+          value={campo}
+          onChange={(e)=>setCampo(e.target.value)}
+          placeholder='Digite sua Tarefa'
+          className="todo-input"
+        />
+        <button type="submit">Adicionar</button>
+      </form>
+
+      <ul>
+        {tarefas.map((tarefa)=>(
+          <li key={tarefa.id}>
+            <span>{tarefa.texto}</span>
+            <button onClick={()=>RemoverTarefa(tarefa.id)}>Excluir</button>
+          </li>
+        ))}
+      </ul>
+      {/* COMPARA SE NÃO TIVER TAREFAS DEIXA A MENSAGEM NENHUMA TAREFA SALVA */}
+      {tarefas.length === 0 && <p>Nenhuma Tarefa Salva</p>}
+      <Contador/>
       
-    </>
+    </div>
   )
 }
 
