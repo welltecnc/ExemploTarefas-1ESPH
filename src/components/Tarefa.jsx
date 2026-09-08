f31943a3b59ff73469cd2e5a41d0b33b5f8d0542
Wellington <welltecnic@hotmail.com>
2026-09-08 11:30:48 -0300

    style:Estilização do projeto com tailwindcss

--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -1,5 +1,4 @@
 import {useState, useEffect} from 'react'
-import Contador from './Contador'
 import '../css/estilo.css'
 
 const Tarefa = () => {
@@ -45,30 +44,40 @@ const Tarefa = () => {
 
 
   return (
-    <div className="todo-container">
-      <h1>Minha Lista de Tarefas</h1>
-      <form onSubmit={AdicionarTarefa}>
+    <div className="max-w-md mx-auto mt-10 bg-indigo-500 rounded-2xl  shadow-lg shadow-amber-200 border border-white">
+      <h1 className="text-2xl font-bold text-white mb-6 text-center">Minha Lista de Tarefas</h1>
+      <form onSubmit={AdicionarTarefa} className="flex gap-2 mb-6">
         <input
           type="text"
           value={campo}
           onChange={(e)=>setCampo(e.target.value)}
           placeholder='Digite sua Tarefa'
-          className="todo-input"
+          className="flex-1 px-4 py-2 border border-gray-700 rounded-2xl focus:outline-none focus:ring-1
+           focus:ring-amber-200 focus:border-transparent text-black placeholder:text-gray-700"
         />
-        <button type="submit">Adicionar</button>
+        <button type="submit"
+          className="bg-indigo-950 hover:bg-indigo-400 text-amber-300  
+          font-medium px-5 py-2 rounded-2xl transition-colors cursor-pointer"
+        >Adicionar</button>
       </form>
 
-      <ul>
+      <ul className="space-y-3">
         {tarefas.map((tarefa)=>(
-          <li key={tarefa.id}>
+          <li key={tarefa.id} 
+          className="flex items-center justify-between p-3 bg-indigo-900 border
+           border-amber-300 rounded-2xl shadow-sm
+            hover:bg-indigo-500 transition-colors" >
             <span>{tarefa.texto}</span>
-            <button onClick={()=>RemoverTarefa(tarefa.id)}>Excluir</button>
+            <button onClick={()=>RemoverTarefa(tarefa.id)}
+            className="bg-red-950 hover:bg-red-400 text-amber-50  
+            font-medium px-5 py-2 rounded-2xl transition-colors cursor-pointer"
+              >Excluir</button>
           </li>
         ))}
       </ul>
       {/* COMPARA SE NÃO TIVER TAREFAS DEIXA A MENSAGEM NENHUMA TAREFA SALVA */}
-      {tarefas.length === 0 && <p>Nenhuma Tarefa Salva</p>}
-      <Contador/>
+      {tarefas.length === 0 && <p className=" text-center italic mt-4">Nenhuma Tarefa Salva</p>}
+    
       
     </div>
   )
